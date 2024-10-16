0fe396b45c0627cddb9217361fb14003c5827410
Aravind6363 <22eg107c63@anurag.edu.in>
2024-10-16 17:33:17 +0530

    Initial commit

---
+++ app_server/controller/locations.js
@@ -0,0 +1,41 @@
+module.exports.homelist = function(req, res) {
+  res.render('locations-list', {
+      title: 'FITZEE',
+      pageHeader: {
+        title: 'DASHBOARD',
+        
+    },
+      sidebar: "Create a tailored fitness and diet plan to optimize health and achieve fitness goals based on Body Mass Index (BMI).",
+      locations: [{
+          name: 'General Fitness Gym',
+          address: 'Ghatkeshar ,Hyderabad',
+          rating: 4,
+          facilities: ['Cardiovascular', 'Strength Training', 'Functional Training','Personal Training and Support Services','Locker Rooms and Showers'],
+          distance: '5km'
+      }, {
+          name: 'BeastMode Gym',
+          address: 'Medipally , Hyderabad',
+          rating: 4,
+          facilities: ['Cardiovascular', 'Strength Training', 'Functional Training','Personal Training and Support Services','Locker Rooms and Showers','Outdoor Facilities'],
+          distance: '20km'
+      }, {
+          name: 'Titan Strength',
+          address: 'Secunderabad , Hyderabad',
+          rating: 4,
+          facilities: ['Cardiovascular', 'Strength Training', 'Functional Training','Personal Training and Support Services','Locker Rooms and Showers','Outdoor Facilities'],
+          distance: '200km'
+  }]
+});
+};
+module.exports.locationInfo = function(req, res){
+    res.render('locations-info1', { title: 'LocationInfo' });
+  };
+module.exports.locationInfo1 = function(req, res){
+    res.render('locations-info2', { title: 'LocationInfo1' });
+  };
+module.exports.locationInfo2 = function(req, res){
+    res.render('locations-info3', { title: 'LocationInfo2' });
+  };
+module.exports.addReview = function(req, res){
+    res.render('location-review', { title: 'ADD REVIEW' });
+  };
