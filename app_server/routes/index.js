0f69db62c032fefbcaacf4e239cbdd0f54fe3952
Aravind6363 <22eg107c63@anurag.edu.in>
2024-10-18 01:28:52 +0530

    first commit

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,20 +1,29 @@
 var express = require('express');
 var router = express.Router();
 var ctrlLocations = require('../controller/locations');
-var ctrlothers = require('../controller/others');
-var ctrlmain = require('../controller/main');
+var ctrlOthers = require('../controller/others');
+var ctrlMain = require('../controller/main');
+
+// Sign-in route
 exports.signin = function(req, res) {
     res.render('signin', { title: 'Sign In' }); // Ensure 'signin' matches your Pug file name
-  }; 
-router.get('/', ctrlLocations.homelist);
+};
+
+// Dashboard route
+router.get('/', ctrlLocations.homelist); // Redirect to the dashboard
+router.post('/calculate', ctrlLocations.calculateBMI); // Handle BMI calculation
+
+// Location information routes
 router.get('/locations', ctrlLocations.locationInfo);
 router.get('/location1', ctrlLocations.locationInfo1);
 router.get('/location/review/new', ctrlLocations.addReview);
-router.get('/about', ctrlothers.about);
-router.get('/signin', ctrlmain.signin)
-router.get('/review', ctrlmain.review)
+
+// Other routes
+router.get('/about', ctrlOthers.about);
+router.get('/signin', ctrlMain.signin);
+router.get('/review', ctrlMain.review);
 router.get('/register', function(req, res, next) {
- res.render('register', { title: 'register' });
+    res.render('register', { title: 'Register' });
 });
- 
+
 module.exports = router;
